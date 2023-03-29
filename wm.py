8d86942e4fc5613d4f796874a11288a33199d29b
WinnMBG <winn77.m@hotmail.com>
2023-03-29 20:08:18 +0200

    add dumb app

--- wm.py
+++ wm.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-   return "<a href='http://localhost:8080/'>Hello, World!</p>"
+   return render_template('index.html')
+   
+@app.route("/<name>/<number>")
+def second(name, number):
+   return render_template('index2.html', name=name, number=int(number))
 
 if __name__== "__main__":
    app.run('0.0.0.0', port=5000)
